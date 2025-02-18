cc329c52e316c5c20deefe65e6668ec19bf3699b
Rudyy Greyrat <quang722008@gmail.com>
2025-02-18 22:29:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Bảo Linh xinh gái",
     introTitle: 'Bảo Linh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    introDesc: `Hello Em bé !!!`,
+    btnIntro: 'Zô',
+    title: 'Em bé có yêu anh hongg 🥰',
+    desc: 'Iu thì chọn nhee',
+    btnYes: 'Iu nhắmmm <33',
+    btnNo: 'Đéo yêu',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Bảo Linh lại yêu anh <3',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Anh biết mà 🥰. Yêu Bảo Linh nhiều nhiều 😘😘',
+    messDesc: 'Iuuuuuu nhắm',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/e2ee/t/100082158890658' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
